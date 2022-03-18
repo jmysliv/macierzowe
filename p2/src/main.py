faa95c630a9aa5b72f23a625a6b082fac5f24ba4
Jakub Mysliwiec <jakub.mysliwiec@swmansion.com>
2022-03-18 21:23:45 +0100

    Added not working inverse algorithm

--- p2/src/main.py
+++ p2/src/main.py
@@ -1,20 +1,18 @@
+import random
+
 ## we received the best results with such value
 L = 2**4
-global add_ctr
-global mul_ctr
+global flop_counter
 
 
 def mul(A, B, C, ax, ay, bx, by, cx, cy, n):
-    global add_ctr
-    global mul_ctr
+    global flop_counter
     if (n <= L):
         for a in range(n):
             for b in range(n):
                 for c in range(n):
-                    if (C[cx + a][cy + b]):
-                        add_ctr = add_ctr + 1
                     C[cx + a][cy + b] += A[ax + a][ay + c] * B[bx + c][bx + b]
-                    mul_ctr = mul_ctr + 1
+                    flop_counter = flop_counter + 2
     else:
         h = int(n/2)
         mul(A, B, C, ax, ay, bx, by, cx, cy, h)
@@ -27,16 +25,76 @@ def mul(A, B, C, ax, ay, bx, by, cx, cy, n):
         mul(A, B, C, ax+h, ay+h, bx+h, by+h, cx+h, cy+h, h)
 
 
-if __name__ == '__main__':
-    k = 7
-    n = 2**k
-    add_ctr = 0
-    mul_ctr = 0
-    A = [[1 for _ in range(n)] for _ in range(n)]
-    B = [[2 for _ in range(n)] for _ in range(n)]
-    C = [[0 for _ in range(n)] for _ in range(n)]
+def inverse(A):
+    global flop_counter
+    n = len(A)
+    if n == 1:
+        if A[0][0] == 0:
+            return [[0]]
+        return [[1/A[0][0]]]
+    h = int(n/2)
+    A11 = [row[:h] for row in A[:h]] 
+    A12 = [row[h:] for row in A[:h]] 
+    A21 = [row[:h] for row in A[h:]] 
+    A22 = [row[h:] for row in A[h:]]
 
-    mul(A, B, C, 0, 0, 0, 0, 0, 0, n)
+    A11_inverse = inverse(A11)
+    A11_inverse_A12 = [[0 for _ in range(h)] for _ in range(h)]
+    mul(A11_inverse, A12, A11_inverse_A12, 0, 0, 0, 0, 0, 0, h)
+    A21_A11_inverse_A12 = [[0 for _ in range(h)] for _ in range(h)]
+    mul(A21, A11_inverse_A12, A21_A11_inverse_A12, 0, 0, 0, 0, 0, 0, h)
+    S22 = [[A22[i][j] - A21_A11_inverse_A12[i][j] for j in range(h)] for i in range(h)]
+    flop_counter = flop_counter + (h**2)
+
+    S22_inverse = inverse(S22)
+    A21_A11_inverse = [[0 for _ in range(h)] for _ in range(h)]
+    mul(A21, A11_inverse, A21_A11_inverse, 0, 0, 0, 0, 0, 0, h)
+
+    A11_inverse_A12_S22_inverse = [[0 for _ in range(h)] for _ in range(h)]
+    mul(A11_inverse_A12, S22_inverse, A11_inverse_A12_S22_inverse, 0, 0, 0, 0, 0, 0, h)
+
+    S22_inverse_A21_A11_inverse = [[0 for _ in range(h)] for _ in range(h)]
+    mul(S22_inverse, A21_A11_inverse, S22_inverse_A21_A11_inverse, 0, 0, 0, 0, 0, 0, h)
+
+    result = [[0 for _ in range(n)] for _ in range(n)]
+    mul(A11_inverse_A12_S22_inverse, A21_A11_inverse, result, 0, 0, 0, 0, 0, 0, h)
+
+    for i in range(n):
+        for j in range(n):
+            # result11
+            if i < h and j < h:
+                result[i][j] += A11_inverse[i][j]
+                flop_counter = flop_counter + 1
+            # result12
+            if i < h and j >= h:
+                result[i][h] = -A11_inverse_A12_S22_inverse[i][j-h]
+            # result21
+            if i >= h and j < h:
+                result[i][j] = -S22_inverse_A21_A11_inverse[i-h][j]
+            # result22
+            if i >= h and j >= h:
+                result[i][j] =  S22_inverse[i-h][j-h]
+
+    return result
+
+
+
+
+
+
+if __name__ == '__main__':
+    k = 2
+    n = 2**k
+    flop_counter = 0
+
+    A = [[random.randint(1, 2) for _ in range(n)] for _ in range(n)]
+
+    print(A)
+    A_inverse = inverse(A)
+    print(A_inverse)
+
+    # checking if works
+    C = [[0 for _ in range(n)] for _ in range(n)]
+    mul(A, A_inverse, C, 0,0,0,0,0,0,n)
     print(C)
-    print(add_ctr)
-    print(mul_ctr)
+    print(flop_counter)
